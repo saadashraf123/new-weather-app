1847eb29a419764c0702b787a64ff2d1190e9ab3
saadashraf123 <saadjewani888@gmail.com>
2023-03-07 19:31:49 +0500

    updating App title

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import DetailPage from './component/DetailPage';
 import FullPageDetails from './component/FullPageDetails';
 import MainPage from './component/MainPage';
-import Weather from './component/Weather';
 
 function App() {
     return (
         <div className='App'>
             <Routes>
-                {/* <Weather /> */}
-                {/* <DetailPage /> */}
                 <Route path="new-weather-app" element={<MainPage />} />
                 <Route path="new-weather-app/fullPageDetails" element={<FullPageDetails />} />
             </Routes>
